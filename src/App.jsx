baeefa8ad45a9ba49aadbc66e767560a249f457c
NjilaBoris <njilaboris2@gmail.com>
2025-08-22 23:09:27 +0100

    Nav Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ReactLenis from "lenis/react";
+gsap.registerPlugin(ScrollTrigger);
 const App = () => {
-  return <div>App</div>;
+  return (
+    <ReactLenis root options={{ duration: 1.2 }}>
+      <div>App</div>
+    </ReactLenis>
+  );
 };
 
 export default App;
