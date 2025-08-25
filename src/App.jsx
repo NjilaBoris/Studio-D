e0ee9c51c155d250d70e06b0a5c6712f52622207
NjilaBoris <njilaboris2@gmail.com>
2025-08-25 23:36:55 +0100

    Nav 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/all";
 import ReactLenis from "lenis/react";
 import Navigation from "./Navigation";
-gsap.registerPlugin(ScrollTrigger);
+import { CustomEase } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger, SplitText, CustomEase);
 const App = () => {
   return (
     <ReactLenis root options={{ duration: 1.2 }}>
-      <div>
+      <div className="container relative">
         <Navigation />
       </div>
     </ReactLenis>
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,9 +1,34 @@
 import React, { useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
 import Container from "./Container";
+import { useGSAP } from "@gsap/react";
+import CustomEase from "gsap/CustomEase";
+import { SplitText } from "gsap/SplitText";
+import gsap from "gsap";
+
 const ITEMS = ["Home", "Project", "Services", "About us", "Contact"];
 const sub = ["ontwerp", "visualisatie", "interactie"];
 const Navigation = () => {
+  // useGSAP(() => {
+  //   CustomEase.create("hop", ".87, 0, .13, 1");
+  //   const textContainer = document.querySelectorAll(".menu-col");
+  //   let splitTextContainer = [];
+  //   textContainer.forEach((container) => {
+  //     const textElement = container.querySelectorAll("li, p, h1, a");
+  //     let containerSplits = [];
+  //     textElement.forEach((element) => {
+  //       const split = SplitText.create(element, {
+  //         type: "lines",
+  //         mask: "lines",
+  //         linesClass: "line",
+  //       });
+  //       containerSplits.push(split);
+  //       gsap.set(split.lines, { y: "-110%" });
+  //     });
+  //     splitTextContainer.push(containerSplits);
+  //   });
+  // }, []);
+  const [open, setOpen] = useState(false);
   const [hovered, setHovered] = useState(0);
   const hover = `relative  after:content-[''] after:absolute after:left-0 after:-bottom-1 
                  after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 
@@ -12,21 +37,50 @@ const Navigation = () => {
     <div className="relative">
       <nav className="fixed w-full z-5">
         <Container>
-          <div className="flex items-center z-2 justify-between relative">
-            <p className="font-normal text-[1.6rem]">Studio D</p>
-            <div className="flex items-center gap-3.5">
-              <p className="font-normal  font-Neue text-neutral-800/90 text-[13.2px]">
+          <div className="flex items-center z-2 justify-between relative menu-bar">
+            <div className="menu-logo">
+              <p className="font-normal text-[1.6rem]">Studio D</p>
+            </div>
+            <div className="flex items-center gap-3.5 overflow-clip">
+              <motion.p
+                layout
+                initial={{ y: 0 }}
+                animate={{ y: open ? "-100%" : 0, opacity: open ? 0 : 1 }}
+                transition={{
+                  type: "spring",
+                  bounce: 0,
+                  duration: 0.5,
+                  ease: "easeInOut",
+                }}
+                className="font-normal  font-Neue text-neutral-800/90 text-[13.2px]"
+              >
                 Menu
-              </p>
-              <div className="bg-black cursor-pointer flex flex-col items-center justify-center size-11  gap-1 p-4 rounded-full">
-                <div className="w-[19px]   h-[1.4px] bg-white" />
-                <div className="w-[19px]  h-[1.4px] bg-white" />
-              </div>
+              </motion.p>
+              <motion.div
+                whileHover={{ scale: 0.95 }}
+                onClick={() => setOpen((open) => !open)}
+                style={{ backgroundColor: open ? "white" : "black" }}
+                className=" cursor-pointer flex transition-colors duration-200 flex-col items-center justify-center size-11  gap-1 p-4 rounded-full"
+              >
+                <div
+                  className="w-[19px]  h-[1.4px] transition-colors duration-150 "
+                  style={{ backgroundColor: open ? "black" : "white" }}
+                />
+                <div
+                  className="w-[19px]  h-[1.4px] transition-colors duration-150  "
+                  style={{ backgroundColor: open ? "black" : "white" }}
+                />
+              </motion.div>
             </div>
           </div>
         </Container>
       </nav>
-      <div className="h-dvh w-full bg-black absolute justify-center flex items-center">
+      <motion.div
+        initial={{ y: "-200%" }}
+        animate={{ y: open ? 0 : "-200%" }}
+        transition={{ type: "spring", bounce: 0, duration: 0.3 }}
+        className="h-[100dvh] w-dvw bg-black absolute inset-0 justify-center flex items-center"
+      >
         <div className="w-full h-full relative">
           <video
             src="/Home.mp4"
@@ -38,7 +92,10 @@ const Navigation = () => {
             <div className="flex items-center gap-2 w-full">
               <div className="flex flex-col  p-2 w-full">
                 {ITEMS.map((item, index) => (
-                  <ul className="font-Polysans overflow-clip   w-fit relative pb-2  font-medium leading-[60px] text-[60px]">
+                  <ul
+                    key={index}
+                    className="menu-col font-Polysans overflow-clip   w-fit relative pb-2  font-medium leading-[60px] text-[60px]"
+                  >
                     <a
                       href="#"
                       key={index}
@@ -55,6 +112,7 @@ const Navigation = () => {
                             transition={{
                               type: "spring",
                               bounce: 0.3,
+                              delay: 0.03,
                               duration: 1,
                               stiffness: 200,
                               damping: 30,
@@ -83,7 +141,7 @@ const Navigation = () => {
                 ))}
               </div>
               <div className="relative  w-full h-[20rem]">
-                <div className="list-none text-left absolute text-[23px] capitalize leading-[30px] right-0 bottom-0 font-Ivar items-end  flex flex-col justify-end">
+                <div className="list-none menu-col text-left absolute text-[23px] capitalize leading-[30px] right-0 bottom-0 font-Ivar items-end  flex flex-col justify-end">
                   {sub.map((item) => (
                     <li className={hover} key={item}>
                       {item}
@@ -93,15 +151,16 @@ const Navigation = () => {
               </div>
             </div>
             <div className="w-full mt-[4rem] font-Neue text-neutral-400 flex items-start justify-between">
-              <h1 className={hover}>Language</h1>
-              <div className="flex text-[1rem] flex-col">
+              <h1 className={`${hover} menu-col`}>Toronto, Canada</h1>
+              <div className="flex text-[1rem] flex-col menu-col">
                 <p className={hover}> +31 (0)26 2344 904</p>
+
                 <p className={hover}>mail@studiod.nu</p>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
